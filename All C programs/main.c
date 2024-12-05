d39b86a3a5f066a9caa1e2d3b90a755862f9126e
SharanuND <sharanudibbadamani2001@gmail.com>
2024-12-05 19:12:18 +0530

    Solved thill 1.5

--- All C programs/main.c
+++ All C programs/main.c
@@ -1,6 +1,44 @@
 // 1
+// #include<stdio.h>
+// void main(){
+//     printf("Hello, World!!!");
+
+// }
+
+// 2
+// #include<stdio.h>
+// void main(){
+//     for(int i=1;i<=100;i++){
+//         printf("%d\t",i);
+//     }
+// }
+
+//3
+// #include<stdio.h>
+// void main(){
+//     for(char i='a';i<='z';i++){
+//         printf("%c ",i);
+//     }
+// }
+
+
+// 4
+// #include<stdio.h>
+// void main(){
+//     for(char i='A';i<='Z';i++){
+//         printf("%c ",i);
+
+//     }
+// }
+
+// 5
 #include<stdio.h>
 void main(){
-    printf("Hello, World!!!");
-    
+    int n;printf("Enter the number : ");scanf("%d",&n);
+
+    if(n%2==0){
+        printf("\nThe number is even");
+    }
+    else
+    printf("\nThe number is ODD\n");
 }
